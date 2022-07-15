d6b9f7c9ae50445bb7a8cb55788a9ffa5f575dd4
HelisonOliveira <helisonoliveira@hotmail.com>
2022-07-15 15:30:23 -0300

    Criando funcionalidade dos operadores

--- Calculadora/api/app.js
+++ Calculadora/api/app.js
@@ -1,11 +1,28 @@
-// import {insertNumber} from './modules/insertNumber'
 
-export const screen = document.getElementById('screen');
+const screen = document.getElementById('screen');
 
-export const numberKeys = document.querySelectorAll('[id*=key]');
+const numberKeys = document.querySelectorAll('[id*=key]');
 
-const updateScreen = (key) => screen.textContent += key 
+const operatorKeys = document.querySelectorAll('[id*= Operator]')
 
-const insertNumbers = insertNumber => updateScreen  (insertNumber.target.textContent)
 
-numberKeys.forEach(numberKey => numberKey.addEventListener('click', insertNumbers));
+const insertNumbers = function insertNumbers(numberKey){
+    return screen.innerText += numberKey.target.innerText
+}
+
+const performOperation = function performOperation(operatorKey){ 
+    if(screen !== 0){
+    return screen.innerText = operatorKey.target.innerText
+    }
+}
+
+numberKeys.forEach(function (numberKey){
+    return numberKey.addEventListener('click',insertNumbers)
+} )
+
+operatorKeys.forEach(function(operatorKey){
+    return operatorKey.addEventListener('click',performOperation)
+})
+
+console.log(numberKeys);
+console.log(operatorKeys);
--- Calculadora/api/modules/insertNumber.js
+++
@@ -1,8 +0,0 @@
-import {screen} from "../app.js"
-
-
-
-export const insertNumber = events => screen.textContent += events;
-
-const screen = document.getElementById('screen');
-console.log(screen);
