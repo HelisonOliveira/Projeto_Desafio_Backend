98c03cce96d24117d85acbae95d53b8e72a4f8c4
HelisonOliveira <helisonoliveira@hotmail.com>
2022-07-16 00:08:42 -0300

    realizando calculos

--- Calculadora/api/app.js
+++ Calculadora/api/app.js
@@ -5,20 +5,53 @@ const numberKeys = document.querySelectorAll('[id*=key]');
 
 const operatorKeys = document.querySelectorAll('[id*= Operator]')
 
+let newNumber = true
+let operator;
+let previousNumber;
 
-const insertNumbers = function insertNumbers(numberKey){
-    return screen.innerText += numberKey.target.innerText
+const pendingOperation = function (){
+    return operator !== undefined
 }
 
-const performOperation = function performOperation(operatorKey){ 
-    if(screen !== 0){
-    return screen.innerText = operatorKey.target.innerText
+const calculateOperation = function (){
+    if(pendingOperation()){
+        const currentNumber = parseFloat(screen.innerText)
+        newNumber = true 
+        if(operator == '+'){
+           return insertNewNumbers(previousNumber + currentNumber)
+        }else if (operator == '-'){
+           return insertNewNumbers(previousNumber - currentNumber)
+        }else if (operator == '*'){
+           return insertNewNumbers(previousNumber * currentNumber)
+        }else if (operator == '/'){
+           return insertNewNumbers(previousNumber / currentNumber)
+        }
     }
+}
+
+const insertNewNumbers = function (anotherNumberKey){
+    if(newNumber){
+        return screen.innerText = anotherNumberKey,
+        newNumber = false
+    } screen.innerText += anotherNumberKey
+}
+const insertNumbers = function insertNumbers(numberKey){
+    return insertNewNumbers(numberKey.target.innerText)   
+    
 }
 
 numberKeys.forEach(function (numberKey){
     return numberKey.addEventListener('click',insertNumbers)
 } )
+
+const performOperation = function performOperation(operatorKey){ 
+    if (!newNumber){ 
+    calculateOperation()
+    newNumber = true,
+    operator = operatorKey.target.innerText,
+    previousNumber = parseFloat(screen.innerText)
+    }
+}
 
 operatorKeys.forEach(function(operatorKey){
     return operatorKey.addEventListener('click',performOperation)
