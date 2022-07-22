e4ae2e99621c316012768f0f56fcba8d13b486f9
HelisonOliveira <helisonoliveira@hotmail.com>
2022-07-22 16:36:02 -0300

    finalizando o 2º desafio Backend

--- Calculadora_IMC/api/app.js
+++ Calculadora_IMC/api/app.js
@@ -1,18 +1,34 @@
-const name = document.getElementById('name')
-
-const height = document.getElementById('height')
-
-const weight = document.getElementById('weight')
 
 const calculate = document.getElementById('button')
 
-const screen = document.getElementById('screen')
+const bodyMassIndex = function CalculateIndex() {
+    const fullName = document.getElementById('name').value
 
-const bodyMassIndex = function CalculateIndex (height, weight){
-    const imc = weight/((height/100)^2)
-    return screen.innerText = imc.toFixed(2)
+    const height = document.getElementById('height').value.replace(',','.')
+
+    const weight = document.getElementById('weight').value.replace(',','.')
+    const screen = document.getElementById('screen')
+
+    if (fullName !== "" && height !== "" && weight !== "") {
+        const imc = (weight / ((height * height))).toFixed(2)
+        let classification = ''
+        if (imc <= 18.5) {
+            classification = 'abaixo do peso.'
+        } else if (imc >= 18.6 && imc <= 24.9) {
+            classification = 'com peso Normal. Parabéns!'
+        } else if (imc >= 25.0 && imc <= 29.9) {
+            classification = 'levemente acima do peso.'
+        } else if (imc >= 30.0 && imc <= 34.9) {
+            classification = 'com Obesidade Grau I.'
+        } else if (imc >= 35.0 && imc <= 39.9) {
+            classification = 'com Obesidade Grau II.'
+        } else if (imc >= 40.0) {
+            classification = 'com Obesidade Morbida. Cuidado!!'
+        }
+        return screen.innerText = `${fullName.toUpperCase()}, seu IMC é ${imc.replace('.', ',')}: Você está ${classification.toUpperCase()}`
+    } return screen.innerText = 'Preencha todos os campos!!!'
 }
 
-bodyMassIndex(170,80)
-// imc = peso/(altura x altura)
+calculate.addEventListener('click', bodyMassIndex)
+
 
